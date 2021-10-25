545b0d0eb70052f71f969bd5ffa09d8149a1e487
Grzegorz Chrymecki <grzegorzchrymecki@gmail.com>
2021-10-25 09:29:06 +0100

    path fixed

--- src/Jwt/Jwt.php
+++ src/Jwt/Jwt.php
@@ -76,11 +76,11 @@ class Jwt
     {
         $path = dirname(__DIR__, 3);
         $isPathFromComposer = str_ends_with($path, '/georgechem');
-        print_r($path);
+
         if($isPathFromComposer){
-            $path = dirname(__DIR__, 2);
-        }else{
             $path = dirname(__DIR__, 5);
+        }else{
+            $path = dirname(__DIR__, 2);
         }
 
         $dotenv = Dotenv::createImmutable($path);
